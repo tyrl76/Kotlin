bc7dd07e605bc36d950549c890c1ba35da61cb06
dongwon <tyrl76@gmail.com>
2023-02-12 21:12:14 +0900

    init lecture

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "fastcampus-kotlin-basic"
+
---
+++ src/main/kotlin/1_Variable.kt
@@ -0,0 +1,27 @@
+var x = 5
+
+fun main() {
+
+    x += 1
+    println(x)
+
+    val a : Int = 1
+
+    val b = 1
+
+    val c : Int
+    c = 3
+
+    val d : Int
+    d = 123
+
+    //val(value) Immutable
+    //var(variable) Mutable
+
+    val e : String = "Hello"
+    // e = "world"
+
+    var f = 123
+    // f = "hi"
+
+}
